e2f67bc1cef40a195f977e2002627b56d06fecfd
Shane GIll <shane@altstore.io>
2022-03-29 16:05:52 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Shane Gill on 3/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  Shared
+//
+//  Created by Shane Gill on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
